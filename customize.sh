3a9266049a529437a677fc8fe8687b1f46ec202e
lordmartian <smeetrs@gmail.com>
2021-11-24 00:07:03 +0530

    Updating customize script

--- customize.sh
+++ customize.sh
@@ -14,12 +14,6 @@
 # ====================================================================
 
 NOCOLOR="\033[0m"
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-BLUE="\033[0;34m"
-PURPLE="\033[0;35m"
-CYAN="\033[0;36m"
 BRED="\033[1;31m"
 BGREEN="\033[1;32m"
 BYELLOW="\033[1;33m"
@@ -31,32 +25,33 @@ BCYAN="\033[1;36m"
 cd ~
 
 # check internet connectivity
-printf "====== CHECKING INTERNET CONNECTION ====== \n"
+printf "$BYELLOW ====== CHECKING INTERNET CONNECTION ====== $NOCOLOR\n"
 ping -c 5 archlinux.org
-printf "====== DONE ====== \n"
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-# enabling time sync
-printf "====== ENABLING TIME SYNC ====== \n"
+# time sync, system update, firewall enable
+printf "$BYELLOW ====== POST-INSTALL TASKS ====== $NOCOLOR\n"
 sudo timedatectl set-ntp true
-printf "====== DONE ====== \n"
-printf "\n"
-sleep 5s
-
-ufw enable
-
-# updating packages
-printf "====== UPDATING PACKAGES ====== \n"
 sudo pacman --noconfirm -Syu
-printf "====== DONE ====== \n"
+sudo ufw enable
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-# ========================= for pure Arch ============================
+# create dirs
+printf "$BYELLOW ====== CREATING EMPTY REQUIRED DIRS ====== $NOCOLOR\n"
+mkdir -p ~/Downloads/Wallpapers
+mkdir -p ~/Downloads/Github
+sudo mkdir -p /mnt/Windows
+sudo mkdir -p /mnt/Data
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
+printf "\n"
+sleep 5s
 
-cd /home/$USER_NAME
-printf "$BYELLOW ====== CHROOT: INSTALLING YAY ====== $NOCOLOR\n"
+# install yay aur helper
+printf "$BYELLOW ====== INSTALLING YAY ====== $NOCOLOR\n"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -sicr
@@ -66,52 +61,41 @@ printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-printf "$BYELLOW ====== CHROOT: INSTALLING ESSENTIAL AUR PACKAGES ====== $NOCOLOR\n"
+# install aur packages
+printf "$BYELLOW ====== INSTALLING AUR PACKAGES ====== $NOCOLOR\n"
 yay -S rtl8821ce-dkms-git pamac-aur nerd-fonts-hack nerd-fonts-fira-code nerd-fonts-jetbrains-mono nerd-fonts-source-code-pro
 printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-# create dirs
-printf "====== CREATING EMPTY REQUIRED DIRS ====== \n"
-mkdir -p ~/Downloads/Wallpapers
-mkdir -p ~/Downloads/Github
-sudo mkdir -p /mnt/Windows
-sudo mkdir -p /mnt/Data
-printf "====== DONE ====== \n"
-printf "\n"
-sleep 5s
-
 # set up git
-printf "====== SETTING UP GIT ====== \n"
+printf "$BYELLOW ====== SETTING UP GIT ====== $NOCOLOR\n"
 git config --global user.email "smeetrs@gmail.com"
 git config --global user.name "lordmartian"
 git config --global credential.helper store
-printf "====== DONE ====== \n"
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
 # zsh
-printf "====== INSTALLING ZSH AND OH-MY-ZSH ====== \n"
+printf "$BYELLOW ====== INSTALLING OH-MY-ZSH ====== $NOCOLOR\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-printf "====== DONE ====== \n"
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-# ============================ GUI ===================================
-
 # github repos
-printf "====== CLONING/DOWNLOADING STUFF FROM GITHUB ====== \n"
+printf "$BYELLOW ====== CLONING/DOWNLOADING STUFF FROM GITHUB ====== $NOCOLOR\n"
 git clone https://github.com/dracula/alacritty.git Downloads/Github/alacritty-dracula
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/tmux-plugins/tpm .tmux/plugins/tpm
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-printf "====== DONE ====== \n"
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
 # dotfiles
-printf "====== ADDING MY DOTFILES ====== \n"
+printf "$BYELLOW ====== ADDING MY DOTFILES ====== $NOCOLOR\n"
 git clone https://github.com/lordmartian/dotfiles.git
 cd dotfiles
 for DIR in *
@@ -125,10 +109,10 @@ do
     fi
 done
 cd ~
-printf "====== DONE ====== \n"
+printf "$BGREEN ====== DONE ====== $NOCOLOR\n"
 printf "\n"
 sleep 5s
 
-printf "====== CUSTOMIZATION COMPLETE. REBOOTING. ====== \n"
+printf "$BBLUE ====== CUSTOMIZATION COMPLETE. REBOOTING. ====== $NOCOLOR\n"
 sleep 5s
-sudo reboot
+sudo reboot now
